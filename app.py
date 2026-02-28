79342872efe6d02ca842b291751992a6f5c72253
agneyra <agneyra63@gmail.com>
2026-02-28 13:46:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ db.init_app(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 # Initialize login manager
 login_manager = LoginManager()
